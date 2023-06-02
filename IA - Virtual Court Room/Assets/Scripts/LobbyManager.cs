0007d3130416c774daceb31a0e01f846d8603504
Frewaran <mert-kara@gmx.de>
2023-06-02 10:40:14 +0200

    Created a way for other players to connect to a room.
    Added a new Prefab "RoomItem". It appears in the Scrollable List, when a room is created. Other players can press on it to join a created room.

--- IA - Virtual Court Room/Assets/Scripts/LobbyManager.cs
+++ IA - Virtual Court Room/Assets/Scripts/LobbyManager.cs
@@ -12,6 +12,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public GameObject roomPanel;
     public Text roomName;
 
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemList = new List<RoomItem>();
+    public Transform contentObject;
+
     private void Start() {
         PhotonNetwork.JoinLobby();
     }
@@ -26,5 +30,26 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(true);
         roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+        UpdateRoomList(roomList);
+    }
+
+    void UpdateRoomList(List<RoomInfo> list) {
+        foreach (RoomItem item in roomItemList) {
+            Destroy(item.gameObject);
+        }
+        roomItemList.Clear();
+
+        foreach (RoomInfo room in list) {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemList.Add(newRoom);
+        }
+    }
+
+    public void JoinRoom(string roomName) {
+        PhotonNetwork.JoinRoom(roomName);
     }
 }
