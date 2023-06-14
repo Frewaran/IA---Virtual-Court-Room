e01d8107efa273c0f20b032085dcbc14360c07e8
JolleHD <jolle1732002@gmail.com>
2023-06-14 16:54:18 +0200

    Door Functions
    JUST CHANGED THE SAMPLE SCENE! (not Multiplayer scene)
    -you can open and close doors by grabbing them or by using the interaction/teleportation ray
    -i used 2 scripts: one for the room- and one for the foyer-doors -> i got Problems with rotation for generall orientations, but for our app it will work
    -added some collider

---
+++ IA - Virtual Court Room/Assets/Scripts/DoorFunctionRoom.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class DoorFunctionRoom : MonoBehaviour
+{
+    private Transform door;
+
+    private XRSimpleInteractable interactable;
+    private bool isDragging = false;
+
+    private Quaternion doorRotCurr;
+    private bool doorIsClosed = false;
+    private bool canToggleDoor = true;
+    private float doorOpenRot;
+
+    public float doorSpeed = 50f;
+
+    public bool doorHingesLeft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = transform;
+        doorOpenRot = door.rotation.eulerAngles.y;
+
+        interactable = GetComponent<XRSimpleInteractable>();
+        interactable.selectEntered.AddListener(StartDrag);
+        interactable.selectExited.AddListener(StopDrag);
+    }
+
+    public void StartDrag(BaseInteractionEventArgs select)
+    {
+        isDragging = true;
+        canToggleDoor = true;
+    }
+
+    private void StopDrag(BaseInteractionEventArgs select)
+    {
+        isDragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isDragging && canToggleDoor) //Interaktion muss begonnen haben und wenn ein Vorgang (z.B. Tür schließen) abgeschlossen ist, muss man erneut Interagieren um diese wieder öffnen zu können (soll nicht durchgehend auf und zu gehen)
+        {
+            if (doorHingesLeft) //Links befestigte Türen
+                openAndCloseLeft();
+            else                //Rechts befestigte Türen
+                openAndCloseRight();
+        }
+
+    }
+
+    private void openAndCloseLeft()
+    {
+        doorRotCurr = door.rotation;
+        Debug.Log(doorRotCurr.eulerAngles.y);
+
+        if (!doorIsClosed) //Wenn die Tür offen ist (wie zu Beginn)
+        {
+            if (doorRotCurr.eulerAngles.y > 90.1f) //Tür wird geschlossen 
+            {
+                door.Rotate(0f, 0f, -doorSpeed * Time.deltaTime);
+            }
+            else if (doorRotCurr.eulerAngles.y < 90f || door.rotation == Quaternion.Euler(270, 90, 0)) //Tür wird genau zu 90 Grad, auch wenn sie etwas zu weit rotiert ist
+            {
+                door.rotation = Quaternion.Euler(270, 90, 0);
+                doorIsClosed = true;
+                canToggleDoor = false;
+            }
+        }
+        else //Wenn die Tür zu ist 
+        {
+            if (doorRotCurr.eulerAngles.y >= 90f && doorRotCurr.eulerAngles.y < 179.9f) //Tür wird geöffnet
+            {
+                door.Rotate(0f, 0f, doorSpeed * Time.deltaTime);
+            }
+            else if (doorRotCurr.eulerAngles.y > 180f || door.rotation == Quaternion.Euler(270, 180, 0)) //Tür wird genau zu 180 Grad, auch wenn sie etwas zu weit rotiert ist
+            {
+                door.rotation = Quaternion.Euler(270, 180, 0);
+                doorIsClosed = false;
+                canToggleDoor = false;
+            }
+        }
+
+
+    }
+
+    private void openAndCloseRight()
+    {
+        doorRotCurr = door.rotation;
+
+        if (!doorIsClosed) //Wenn die Tür offen ist (wie zu Beginn)
+        {
+            if (doorRotCurr.eulerAngles.y < 89.1f && doorRotCurr.eulerAngles.y >= 0f) //Tür wird geschlossen 
+            {
+                door.Rotate(0f, 0f, doorSpeed * Time.deltaTime);
+            }
+            else if (doorRotCurr.eulerAngles.y > 90f || door.rotation == Quaternion.Euler(270, 90, 0)) //Tür wird genau zu 0 Grad, auch wenn sie etwas zu weit rotiert ist
+            {
+                door.rotation = Quaternion.Euler(270, 90, 0);
+                doorIsClosed = true;
+                canToggleDoor = false;
+            }
+        }
+        else //Wenn die Tür zu ist 
+        {
+            if (doorRotCurr.eulerAngles.y <= 90f && doorRotCurr.eulerAngles.y > 0.1f) //Tür wird geöffnet
+            {
+                door.Rotate(0f, 0f, -doorSpeed * Time.deltaTime);
+            }
+            else if (doorRotCurr.eulerAngles.y > 0f && doorRotCurr.eulerAngles.y > 90f || door.rotation == Quaternion.Euler(270, 0, 0))
+            {
+                door.rotation = Quaternion.Euler(270, 0, 0);
+                doorIsClosed = false;
+                canToggleDoor = false;
+            }
+        }
+
+
+    }
+
+    //doorAngle = doorAngle > 180f ? doorAngle - 360f : doorAngle; // Normalisiere den Winkel auf den Bereich von -180 bis 180 Grad
+
+}
