d7f3083423e21138b07a7fccaa38ba6f2e1eebfc
aeiben-th <131672515+aeiben-th@users.noreply.github.com>
2023-08-07 11:52:07 +0200

    Avatar Player Model fix
    Fixed bug in avatar controller
    
    Aligned RPM-model with VR-rig
    
    Disabled VR-hands render component

--- IA - Virtual Court Room/Assets/Scripts/AvatarController.cs
+++ IA - Virtual Court Room/Assets/Scripts/AvatarController.cs
@@ -1,41 +1,36 @@
 using UnityEngine;
 
 [System.Serializable]
-
-public class MapTransforms
+public class MapTransform
 {
     public Transform vrTarget;
-    public Transform ikTarget;
-
+    public Transform IkTarget;
     public Vector3 trackingPositionOffset;
     public Vector3 trackingRotationOffset;
 
-    public void VRMapping()
+    public void MapVrAvatar()
     {
-        ikTarget.position = vrTarget.InverseTransformPoint(trackingPositionOffset);
-        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+        IkTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        IkTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
-
 }
 
 public class AvatarController : MonoBehaviour
 {
-    [SerializeField] private MapTransforms head;
-    [SerializeField] private MapTransforms leftHand;
-    [SerializeField] private MapTransforms rightHand;
+    [SerializeField] private MapTransform head;
+    [SerializeField] private MapTransform leftHand;
+    [SerializeField] private MapTransform rightHand;
 
     [SerializeField] private float turnSmoothness;
-    [SerializeField] Transform ikHead;
-    [SerializeField] Vector3 headBodyOffset;
+    [SerializeField] private Transform IkHead;
+    [SerializeField] private Vector3 headBodyOffset;
 
     private void LateUpdate()
     {
-        transform.position = ikHead.position + headBodyOffset;
-        //transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(ikHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
-
-        head.VRMapping();
-        leftHand.VRMapping();
-        rightHand.VRMapping();
+        transform.position = IkHead.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(IkHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
+        head.MapVrAvatar();
+        leftHand.MapVrAvatar();
+        rightHand.MapVrAvatar();
     }
-
 }
