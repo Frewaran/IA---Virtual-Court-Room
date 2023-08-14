79368373c2d1364d468ce1d833ce1ef2a80e0202
Frewaran <mert-kara@gmx.de>
2023-08-14 14:29:18 +0200

    Voice Recorder ..

--- IA - Virtual Court Room/Assets/Scripts/AudioRecorder.cs
+++ IA - Virtual Court Room/Assets/Scripts/AudioRecorder.cs
@@ -4,36 +4,12 @@ using UnityEngine;
 
 public class AudioRecorder : MonoBehaviour
 {
-    AudioSource audioSource;
-    private string microphoneName = Microphone.devices[0];
-    bool recorded = false;
-
-    private void Awake() {
-        audioSource = GetComponent<AudioSource>();
-        //foreach(var mic in Microphone.devices) {
-          //  Debug.Log(mic);
-        //}
-    }
-
     public void StartRecordingAudio() {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        string microphoneName = Microphone.devices[0];
+
         audioSource.clip = Microphone.Start(microphoneName, true, 5, 44100);
         audioSource.Play();
         Debug.Log("Start");
     }
-    /*
-    public void StopRecordingAudio() {
-        Microphone.End(microphoneName);
-        recorded = true;
-        Debug.Log("Stop");
-    }
-
-    private void Update() {
-        
-        if (recorded) {
-            audioSource.Play();
-
-            Debug.Log("Recorded");
-        }
-    }
-    */
 }
