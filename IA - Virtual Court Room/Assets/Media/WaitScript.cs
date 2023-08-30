d6a8bd7571ff4b778a487a848ab6df736a9afef3
Firasfby <132066234+Firasfby@users.noreply.github.com>
2023-08-30 12:30:36 +0200

    Wait Script angepasst + Zurückbutton neu eingestellt

--- IA - Virtual Court Room/Assets/Media/WaitScript.cs
+++ IA - Virtual Court Room/Assets/Media/WaitScript.cs
@@ -1,32 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
     public class WaitScript:MonoBehaviour
     {
+        public Button MenuButton;
         public GameObject UiElement;
     // Start is called before the first frame update
     void Start()
     {
-     StartCoroutine(WaitForFunction());
+    Button btn = MenuButton.GetComponent<Button>();
+	btn.onClick.AddListener(TaskOnClick);
+   
     }
   public IEnumerator WaitForFunction()
     {
         Debug.Log("Hello?");
-        yield return new WaitForSeconds(10);
-        
-        UiElement.SetActive(true); 
-       
-    }
+        yield return new WaitForSeconds(1);
+          UiElement.SetActive(true);
+       } 
+    
+    
     
     // Update is called once per frame
     void Update()
     { 
  
     }
-
-    public void Click(){
-     Debug.Log("HalloWelt");
-     UiElement.SetActive(true);  
+    void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+         StartCoroutine(WaitForFunction());
+    
+	}
     }
-}
+
+
+
