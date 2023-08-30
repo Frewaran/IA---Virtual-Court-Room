58e19f8475940ebf1986077a5de9054b3e56b994
Frewaran <34106362+Frewaran@users.noreply.github.com>
2023-08-30 11:58:59 +0200

    Disabled everything xD

--- IA - Virtual Court Room/Assets/NewNetworkPlayer.cs
+++ IA - Virtual Court Room/Assets/NewNetworkPlayer.cs
@@ -6,6 +6,7 @@ using System;
 using ReadyPlayerMe.AvatarLoader;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Animations.Rigging;
 
 public class NewNetworkPlayer : MonoBehaviour {
 
@@ -14,19 +15,34 @@ public class NewNetworkPlayer : MonoBehaviour {
     private void Awake() {
         photonView = GetComponent<PhotonView>();
     }
-    private void Update() {
+    private void Start() {
         if(!photonView.IsMine) {
             GetComponent<XROrigin>().enabled = false;
             GetComponent<InputActionManager>().enabled = false;
-            GetComponent<LocomotionSystem>().enabled = false;
             GetComponent<ActionBasedContinuousMoveProvider>().enabled = false;
             GetComponent<ActionBasedContinuousTurnProvider>().enabled = false;
             GetComponent<ActionBasedSnapTurnProvider>().enabled = false;
-            GetComponent<CharacterControllerDriver>().enabled = false;
+            GetComponent<LocomotionSystem>().enabled = false;
             GetComponent<CharacterController>().enabled = false;
+            GetComponent<CharacterControllerDriver>().enabled = false;
             GetComponent<TeleportationProvider>().enabled = false;
             GetComponent<ActivateTeleportationRay>().enabled = false;
-            GetComponent<Minimap>().enabled = false;
+
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(false);
+            transform.GetChild(3).gameObject.SetActive(false);
+            transform.GetChild(4).gameObject.SetActive(false);
+            transform.GetChild(5).gameObject.SetActive(false);
+
+            GetComponentInChildren<AvatarData>().enabled = false;
+            GetComponentInChildren<EyeAnimationHandler>().enabled = false;
+            GetComponentInChildren<AvatarController>().enabled = false;
+            GetComponentInChildren<RigBuilder>().enabled = false;
+            GetComponentInChildren<BoneRenderer>().enabled = false;
+            GetComponentInChildren<AnimateHandOnInput>().enabled = false;
+            GetComponentInChildren<LowerBodyAnimator>().enabled = false;
+            GetComponentInChildren<AnimationController>().enabled = false;
+            GetComponentInChildren<AnimationController>().enabled = false;
         }
     }
 }
